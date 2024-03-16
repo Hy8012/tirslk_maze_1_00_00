8052f588629d9121a1475cf1e0028af92c43209e
Henry <152554805+Hy8012@users.noreply.github.com>
2024-03-16 00:13:23 -0700

    IO, IO-ADEN, GPIO

---
+++ Lab06_GPIO/Debug/Lab06_GPIOmain.d
@@ -0,0 +1,75 @@
+# FIXED
+
+Lab06_GPIOmain.obj: ../Lab06_GPIOmain.c
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/ccs_base/arm/include/msp.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/ccs_base/arm/include/msp_compatibility.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r_classic.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+Lab06_GPIOmain.obj: /Applications/ti/ccs1200/ccs/ccs_base/arm/include/system_msp432p401r.h
+Lab06_GPIOmain.obj: ../../inc/Reflectance.h
+Lab06_GPIOmain.obj: ../../inc/Clock.h
+Lab06_GPIOmain.obj: ../../inc/CortexM.h
+Lab06_GPIOmain.obj: ../../inc/LaunchPad.h
+Lab06_GPIOmain.obj: ../../inc/SysTickInts.h
+
+../Lab06_GPIOmain.c:
+
+/Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h:
+
+/Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h:
+
+/Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h:
+
+/Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h:
+
+/Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h:
+
+/Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h:
+
+/Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h:
+
+/Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h:
+
+/Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h:
+
+/Applications/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h:
+
+/Applications/ti/ccs1200/ccs/ccs_base/arm/include/msp.h:
+
+/Applications/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r.h:
+
+/Applications/ti/ccs1200/ccs/ccs_base/arm/include/msp_compatibility.h:
+
+/Applications/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r_classic.h:
+
+/Applications/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/core_cm4.h:
+
+/Applications/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h:
+
+/Applications/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h:
+
+/Applications/ti/ccs1200/ccs/ccs_base/arm/include/system_msp432p401r.h:
+
+../../inc/Reflectance.h:
+
+../../inc/Clock.h:
+
+../../inc/CortexM.h:
+
+../../inc/LaunchPad.h:
+
+../../inc/SysTickInts.h:
+
---
+++ Lab06_GPIO/Debug/subdir_vars.mk
@@ -0,0 +1,69 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+/Users/henry/git/rslk-aden-yosth-mvla/tirslk_maze_1_00_00/inc/Clock.c \
+/Users/henry/git/rslk-aden-yosth-mvla/tirslk_maze_1_00_00/inc/CortexM.c \
+../Lab06_GPIOmain.c \
+/Users/henry/git/rslk-aden-yosth-mvla/tirslk_maze_1_00_00/inc/LaunchPad.c \
+/Users/henry/git/rslk-aden-yosth-mvla/tirslk_maze_1_00_00/inc/Reflectance.c \
+/Users/henry/git/rslk-aden-yosth-mvla/tirslk_maze_1_00_00/inc/SysTickInts.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./Clock.d \
+./CortexM.d \
+./Lab06_GPIOmain.d \
+./LaunchPad.d \
+./Reflectance.d \
+./SysTickInts.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./Clock.obj \
+./CortexM.obj \
+./Lab06_GPIOmain.obj \
+./LaunchPad.obj \
+./Reflectance.obj \
+./SysTickInts.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"Clock.obj" \
+"CortexM.obj" \
+"Lab06_GPIOmain.obj" \
+"LaunchPad.obj" \
+"Reflectance.obj" \
+"SysTickInts.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"Clock.d" \
+"CortexM.d" \
+"Lab06_GPIOmain.d" \
+"LaunchPad.d" \
+"Reflectance.d" \
+"SysTickInts.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"/Users/henry/git/rslk-aden-yosth-mvla/tirslk_maze_1_00_00/inc/Clock.c" \
+"/Users/henry/git/rslk-aden-yosth-mvla/tirslk_maze_1_00_00/inc/CortexM.c" \
+"../Lab06_GPIOmain.c" \
+"/Users/henry/git/rslk-aden-yosth-mvla/tirslk_maze_1_00_00/inc/LaunchPad.c" \
+"/Users/henry/git/rslk-aden-yosth-mvla/tirslk_maze_1_00_00/inc/Reflectance.c" \
+"/Users/henry/git/rslk-aden-yosth-mvla/tirslk_maze_1_00_00/inc/SysTickInts.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
